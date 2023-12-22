60be1d519ddef4b35c17416ee4bb4205d6a7e9b6
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2023-12-22 17:37:57 -0300

    Update BETAEngine.js

--- BETAEngine/BETAEngine.js
+++ BETAEngine/BETAEngine.js
@@ -1,19 +1,57 @@
-import {Player} from "./Player.js"
-import {Map} from "./Map.js"
+import {Player} from "./Player.js";
 
-export let Player = new Player;
-var Spacecraft = new Map(8, 8, new Array (
-    1,1,1,1,1,1,1,1,
-    1,0,1,0,0,0,0,1,
-    1,0,1,0,1,1,0,1,
-    1,0,0,0,0,1,0,1,
-    1,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,1,
-    1,0,0,0,0,1,0,1,
-    1,1,1,1,1,1,1,1
-));
+let WIDTH = 640;
+let HEIGHT = 448;
 
-export function Render() {
-    Spacecraft.DrawMap2d();
-    Player.DrawRays3d();
+let MAP_SIZE = 0;
+let MAP = [];
+
+const WALL_HEIGHT = 50;
+export let Player = new Player();
+
+export function SetMap(size, newmap) {
+  MAP_SIZE = size;
+  MAP = newmap;
+}
+
+function castRay(angle) {
+    var x = Player.x;
+    var y = Player.y;
+    var stepX = Math.cos(angle);
+    var stepY = Math.sin(angle);
+
+    while (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+        var mapX = Math.floor(x / (WIDTH / MAP_SIZE));
+        var mapY = Math.floor(y / (HEIGHT / MAP_SIZE));
+
+        if (map[mapY][mapX] === 1) {
+            var distance = Math.sqrt((x - Player.x) ** 2 + (y - Player.y) ** 2);
+            var wallHeight = WALL_HEIGHT / distance * Math.cos(angle - Player.angle);
+            return {
+                distance,
+                wallHeight,
+                hitWall: true
+            };
+        }
+        x += stepX;
+        y += stepY;
+    }
+
+    return {
+        distance: Infinity,
+        wallHeight: 0,
+        hitWall: false
+    };
+}
+
+export function render() {
+    for (let col = 0; col < WIDTH; col++) {
+        var rayAngle = Player.angle - FOV / 2 + (col / WIDTH) * FOV;
+        var { distance, wallHeight, hitWall } = castRay(rayAngle);
+
+        var lineHeight = (WALL_HEIGHT * HEIGHT) / (distance * Math.cos(rayAngle - Player.angle));
+        var wallTop = (HEIGHT - lineHeight) / 2;
+
+        Draw.rect(coll, wallTop, 1, lineHeight, hitWall ? Color.new(40, 40, 40) : Color.new(0, 0, 0));
+    }
 }
